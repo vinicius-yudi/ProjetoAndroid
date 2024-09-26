324609ad2acb6390c6fffea81ebea8208ef986b4
Vinicius Yudi <vinizzly2005@gmail.com>
2024-09-26 02:31:25 -0300

    Tela de Login e começo de outra tela

---
+++ app/src/main/java/com/yudi/projetoandroid/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.yudi.projetoandroid
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.yudi.projetoandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        window.statusBarColor= Color.parseColor("#FFFFFF")
+
+        binding.btEntrar.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                email.isEmpty() -> {
+                    binding.editEmail.error = "Preencha o E-mail!"
+                }
+                senha
+                    .isEmpty() -> {
+                        binding.editSenha.error = "Preencha a Senha!"
+                    }
+                !email.contains("@gmail.com") -> {
+                    val snackbar = Snackbar.make(it,"E-mail inválido!", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                senha.length <= 5 -> {
+                    val snackbar = Snackbar.make(it,"A senha deve ter pelo menos 6 caracteres!", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                else -> {
+                    login(it)
+                }
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+    private fun login(view: View){
+
+        val progressbar = binding.progressBar
+        progressbar.visibility = View.VISIBLE
+
+        binding.btEntrar.isEnabled = false
+        binding.btEntrar.setTextColor(Color.parseColor("#FFFFFF"))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navegarTelaPrincipal()
+            val snackbar = Snackbar.make(view,"A senha deve ter pelo menos 6 caracteres!", Snackbar.LENGTH_SHORT)
+            snackbar.show()
+        },3000)
+    }
+
+    private fun navegarTelaPrincipal(){
+        val intent = Intent(this,TelaPrincipal::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+
+}
