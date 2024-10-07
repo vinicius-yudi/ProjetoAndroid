dae836ea611d5099b4c88584b974bd931def73c1
gdserafini <gdinizserafini@gmail.com>
2024-10-07 20:42:37 -0300

    add night mode -> game

--- app/src/main/java/com/yudi/projetoandroid/TelaGame.kt
+++ app/src/main/java/com/yudi/projetoandroid/TelaGame.kt
@@ -1,21 +1,37 @@
 package com.yudi.projetoandroid
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
 
 class TelaGame : AppCompatActivity() {
 
     private lateinit var spinner: Spinner
     private val db = MeuBancoDeDados(this)
+    private var isDark = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_game)
+
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+        val toggleButton = findViewById<Button>(R.id.btn_toggle_color)
+
+        toggleButton.setOnClickListener {
+            if (isDark) {
+                mainLayout.setBackgroundColor(Color.parseColor("#CCCCCC"))
+            } else {
+                mainLayout.setBackgroundColor(Color.BLACK)
+            }
+            isDark = !isDark
+        }
 
         // Inicializa o Spinner após o setContentView
         spinner = findViewById(R.id.spinnerTelefone)
